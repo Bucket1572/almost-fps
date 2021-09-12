3a80bd140e392e36f91b9b78c45c5f5544cd2702
Bucket1572 <sout0811@naver.com>
2021-09-12 22:03:29 +0900

    Settings: 초기 세팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
     compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
 
     implementation(kotlin("stdlib"))
+    implementation("io.github.monun:kommand-api:2.6.6")
+    implementation("io.github.monun:tap-api:4.1.2")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.2")
---
+++ src/main/kotlin/io/github/bucket1572/plugin/fpsishPlugin.kt
@@ -0,0 +1,9 @@
+package io.github.bucket1572.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class fpsishPlugin : JavaPlugin() {
+    override fun onEnable() {
+        // 초기화
+    }
+}
